79c862f6bd81b7e4380f164c9c7bc0ecff3bff8f
hdp0545 <hdp0545@naver.com>
2020-08-10 15:35:16 +0900

    Add vue

--- vue_spring/frontend/src/components/VideoPlayerVue.vue
+++ vue_spring/frontend/src/components/VideoPlayerVue.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="player">
     <video id="video" controls width="500px" preload="metadata">
+      <track label="korean" kind="subtitles" srclang="ko" src="sftp://ubuntu@i3a511.p.ssafy.io/home/ubuntu/resources/vtt/vczvcxzfsda.vtt" default>
+      <track label="english" kind="subtitles" srclang="en" src="sftp://ubuntu@i3a511.p.ssafy.io/home/ubuntu/resources/vtt/vczvcxzfsda.vtt">
       <source :src="objectUrl" type="video/mp4">
     </video>
+    <button class="btn btn-primary" @click="fileDownload">다운로드</button>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
+const SERVER_URL = 'http://i3a511.p.ssafy.io:8399'
+
 export default {
   name: 'video-player-vue',
   props: {
@@ -14,6 +21,20 @@ export default {
   },
   data() {
     return {
+    }
+  },
+  methods:{
+    fileDownload() {
+      axios.get(`${SERVER_URL}/api/download`)
+      .then((response) => {
+        const fileURL = window.URL.createObjectURL(new Blob([response.data]))
+        const fileLink = document.createElement('a')
+        fileLink.href = fileURL
+        fileLink.setAttribute('download', 'file.vtt')
+        document.body.appendChild(fileLink)
+
+        fileLink.click()
+      })
     }
   },
   computed: {
