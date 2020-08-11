4702539fac9f5a5ba021d58d4c8c195d94dc3ad9
hdp0545 <hdp0545@naver.com>
2020-08-11 15:12:45 +0900

    Add subtitle Download

--- vue_spring/frontend/src/components/VideoPlayerVue.vue
+++ vue_spring/frontend/src/components/VideoPlayerVue.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="player">
-    <video id="video" controls width="500px" preload="metadata">
-      <track label="korean" kind="subtitles" srclang="ko" src="sftp://ubuntu@i3a511.p.ssafy.io/home/ubuntu/resources/vtt/vczvcxzfsda.vtt" default>
-      <track label="english" kind="subtitles" srclang="en" src="sftp://ubuntu@i3a511.p.ssafy.io/home/ubuntu/resources/vtt/vczvcxzfsda.vtt">
+    <video id="video" controls class="col-12" width="500px" preload="metadata" crossorigin="anonymous">
       <source :src="objectUrl" type="video/mp4">
+      <track label="korean" kind="subtitles" srclang="ko" :src="downloadUrl" default>
+      <track label="english" kind="subtitles" srclang="en" :src="downloadUrl">
+      <track label="english1" kind="subtitles" srclang="ko" :src=downloadUrl>
+      <track label="english2" kind="subtitles" srclang="en" :src=downloadUrl>
     </video>
-    <button class="btn btn-primary" @click="fileDownload">다운로드</button>
+    <a :href="downloadUrl"> 자막 다운로드 </a>
+    <a :href=downloadUrl> 자막 다운로드 </a>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
-
-const SERVER_URL = 'http://i3a511.p.ssafy.io:8399'
 
 export default {
   name: 'video-player-vue',
@@ -24,22 +24,16 @@ export default {
     }
   },
   methods:{
-    fileDownload() {
-      axios.get(`${SERVER_URL}/api/download`)
-      .then((response) => {
-        const fileURL = window.URL.createObjectURL(new Blob([response.data]))
-        const fileLink = document.createElement('a')
-        fileLink.href = fileURL
-        fileLink.setAttribute('download', 'file.vtt')
-        document.body.appendChild(fileLink)
 
-        fileLink.click()
-      })
-    }
   },
   computed: {
     objectUrl() {
       return URL.createObjectURL(this.video)
+    },
+    downloadUrl() {
+      const name = this.video.name.replace(".mp4", "")
+      const url = "http://i3a511.p.ssafy.io/api/vtt/download?fileLink=" + name 
+      return url
     }
   }
 }
