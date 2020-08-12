3864d11352a176c643a4e793bad6d7e192adb0e9
hdp0545 <hdp0545@naver.com>
2020-08-12 15:25:29 +0900

    Add Drag&Drop

--- vue_spring/frontend/src/components/UploadFiles.vue
+++ vue_spring/frontend/src/components/UploadFiles.vue
@@ -1,58 +1,63 @@
 <template>
-  <div>
-    <img class="img-video" src='../img/icon-video.png' alt="비디오 로고" height="100"/>
-    <label class="selectbtn btn btn-default">
-      <!-- <input type="file"  /> -->
-      <input type="text" value="선택된 파일이 없습니다." id="fileName" class="file_input_textbox" readonly="readonly">
-      <input type="button" value="파일 선택" class="file_input_button font-weight-bold"/>
-      <input type="file" ref="file" @change="selectFile" class="file_input_hidden" onchange="javascript:document.getElementById('fileName').value = this.value.split('\\')[this.value.split('\\').length-1]">
-    </label>
-    <!-- :disabled="!selectedFiles" -->
-    <div>
-      <b-button v-b-modal.show-btn class="uploadbtn btn btn-dark" @click="$bvModal.show('bv-modal-example')">
-        자막 변환
-      </b-button>
-      <!-- 모달 창 -->
-      <b-modal id="bv-modal-example" hide-footer>
-        <!-- 헤더 -->
-        <template v-slot:modal-title>언어 선택</template>
-        <p style="font-size: 15px;">해당 영상의 언어와 자막으로 번역하고자 하는 언어를 설정해주세요.</p>
-        <div class="d-flex justify-content-around">
-          <p><b-button variant="light" size="lg" squared v-b-popover.hover.top="'해당 영상의 언어를 선택해주세요'" title="음성 언어" style="font-weight: bold;">
-            음성</b-button></p>
-          <p><b-button variant="light" size="lg" squared v-b-popover.hover.top="'자막으로 번역하고자 하는 언어를 선택해주세요'" title="자막 언어" style="font-weight: bold;">
-            자막</b-button></p>
-        </div>
-        <!-- 언어 선택 -->
-        <div class="d-flex justify-content-around">
-          <b-form-select v-model="uploadData.start" :options="uploadData.option1"></b-form-select>
-          <b-form-select v-model="uploadData.target" :options="uploadData.option2"></b-form-select>
-        </div>
-        <div class="d-flex justify-content-around">
-          <div class="mt-3">음성 언어: <strong>{{ uploadData.start }}</strong></div>
-          <div class="mt-3">자막 언어: <strong>{{ uploadData.target }}</strong></div>
-        </div>
-        <div class="d-flex justify-content-around">
-          <!-- 링크 연결된 upload 버튼 -->
-          <b-button block squared variant="primary" class="mt-3" @click="uploadOption" style="font-weight: bold;">변환 시작</b-button>
-          <!-- <b-button squared class="mt-3" @click="$bvModal.hide('bv-modal-example')">취소</b-button> -->
-        </div>
-        <hr>
-        <!-- <p class="text-center" style="color: black;">영상을 번역하고 있습니다.</p> -->
-        <div v-if="currentFile" class="progress">
-          <div
-            class="progress-bar progress-bar-info progress-bar-striped"
-            role="progressbar"
-            :aria-valuenow="progress"
-            aria-valuemin="0"
-            aria-valuemax="100"
-            :style="{ width: progress + '%' }"
-          >
-            {{ progress }}%
+  <div @drop.prevent="addFile" @dragover.prevent>
+    <div class="container" style="position: absolute;">
+      <img src=".\..\img\upload-box.png" alt="업로드 박스" class="upload-box">
+    </div>
+    <div class="upload-content" style="position: absolute;">
+      <img class="img-video" src='../img/icon-video.png' alt="비디오 로고" height="100"/>
+      <label class="selectbtn btn btn-default">
+        <!-- <input type="file"  /> -->
+        <input type="text" value="선택된 파일이 없습니다." id="fileName" class="file_input_textbox" readonly="readonly">
+        <input type="button" value="파일 선택" class="file_input_button font-weight-bold"/>
+        <input type="file" ref="file" @change="selectFile" class="file_input_hidden" onchange="javascript:document.getElementById('fileName').value = this.value.split('\\')[this.value.split('\\').length-1]">
+      </label>
+      <!-- :disabled="!selectedFiles" -->
+      <div>
+        <b-button v-b-modal.show-btn class="uploadbtn btn btn-dark" @click="$bvModal.show('bv-modal-example')">
+          자막 변환
+        </b-button>
+        <!-- 모달 창 -->
+        <b-modal id="bv-modal-example" hide-footer>
+          <!-- 헤더 -->
+          <template v-slot:modal-title>언어 선택</template>
+          <p style="font-size: 15px;">해당 영상의 언어와 자막으로 번역하고자 하는 언어를 설정해주세요.</p>
+          <div class="d-flex justify-content-around">
+            <p><b-button variant="light" size="lg" squared v-b-popover.hover.top="'해당 영상의 언어를 선택해주세요'" title="음성 언어" style="font-weight: bold;">
+              음성</b-button></p>
+            <p><b-button variant="light" size="lg" squared v-b-popover.hover.top="'자막으로 번역하고자 하는 언어를 선택해주세요'" title="자막 언어" style="font-weight: bold;">
+              자막</b-button></p>
           </div>
-        </div>
-      </b-modal>
+          <!-- 언어 선택 -->
+          <div class="d-flex justify-content-around">
+            <b-form-select v-model="uploadData.start" :options="uploadData.option1"></b-form-select>
+            <b-form-select v-model="uploadData.target" :options="uploadData.option2"></b-form-select>
+          </div>
+          <div class="d-flex justify-content-around">
+            <div class="mt-3">음성 언어: <strong>{{ uploadData.start }}</strong></div>
+            <div class="mt-3">자막 언어: <strong>{{ uploadData.target }}</strong></div>
+          </div>
+          <div class="d-flex justify-content-around">
+            <!-- 링크 연결된 upload 버튼 -->
+            <b-button block squared variant="primary" class="mt-3" @click="uploadOption" style="font-weight: bold;">변환 시작</b-button>
+            <!-- <b-button squared class="mt-3" @click="$bvModal.hide('bv-modal-example')">취소</b-button> -->
+          </div>
+          <hr>
+          <!-- <p class="text-center" style="color: black;">영상을 번역하고 있습니다.</p> -->
+          <div v-if="currentFile" class="progress">
+            <div
+              class="progress-bar progress-bar-info progress-bar-striped"
+              role="progressbar"
+              :aria-valuenow="progress"
+              aria-valuemin="0"
+              aria-valuemax="100"
+              :style="{ width: progress + '%' }"
+            >
+              {{ progress }}%
+            </div>
+          </div>
+        </b-modal>
 
+      </div>
     </div>
 
   </div>
@@ -94,6 +99,10 @@ export default {
       console.log("exex")
       this.selectedFiles = this.$refs.file.files
     },
+    addFile(e) {
+      this.selectedFiles = e.dataTransfer.files;
+
+    },
     uploadOption() {
       console.log("start")
       this.progress = 0;
@@ -104,6 +113,8 @@ export default {
         this.progress = Math.round((100 * event.loaded) / event.total);
       })
         .then(response => {
+          console.log("잘 오니??")
+          console.log(response)
           this.message = response.data.message;
           this.$emit('submit-upload-option', this.uploadData)
           // return UploadService.getFiles();
@@ -164,6 +175,18 @@ filter:alpha(opacity=0);
 -ms-filter:"alpha(opacity=0)"; 
 -khtml-opacity:0; 
 -moz-opacity:0;}
+
+
+.upload-box {
+  margin-top: 100px;
+  margin-right: 500px;
+  width:750px 
+}
+
+.upload-content {
+  margin-top: 500px;
+  margin-left: -1100px;
+}
 
 
 </style>
