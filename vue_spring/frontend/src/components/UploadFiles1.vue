99a2f5568a14368f99e5c03a692bdad474893a47
홍지선 <zs7819@naver.com>
2020-08-13 19:53:52 +0900

    Edit modal upload

---
+++ vue_spring/frontend/src/components/UploadFiles1.vue
@@ -0,0 +1,39 @@
+<template>
+  <div role="group">
+      <label for="input-live"></label>
+      <b-form-input
+        id="input-live"
+        v-model="link"
+        :state="LinkState"
+        aria-describedby="input-live-help input-live-feedback"
+        placeholder="예시) https://www.youtube.com/watch?v="
+        trim
+      ></b-form-input>
+
+      <b-button v-b-modal.show-btn class="uploadbtn btn btn-warning" 
+        @click="$bvModal.show('bv-modal-example')">
+          자막 생성 시작
+        </b-button>
+
+      <!-- <b-form-text id="input-live-help">유투브 영상 링크를 입력해주세요.</b-form-text> -->
+    </div>
+</template>
+
+<script>
+export default {
+  name: "upload-files1",
+
+}
+</script>
+
+<style>
+.form-control {
+    display: block;
+    width: 50px;
+}
+
+.uploadbtn {
+  margin: 10px 0px;
+}
+
+</style>
--- vue_spring/frontend/src/views/contents/CreateCaption.vue
+++ vue_spring/frontend/src/views/contents/CreateCaption.vue
@@ -1,95 +1,76 @@
 <template>
   <div class="wrapper">
     <!-- Sidebar  -->
-    <nav id="sidebar">
-      <div class="sidebar-header">
-        <button type="button" id="sidebarCollapse" class="btn">
-          <i class="fa fa-bars fa-inverse"></i>
-        </button>
+    <div class="container-fluid m-0 p-0" style="height:100vh;">
+    <div class="row">
+      <div class="col-2" style="background-color:#564892;">
+        <b-list-group style="height:100vh; border-style:none;">
+          <b-list-group-item href="/" class="text-white pt-3 mb-2" style="background-color:#564892; height:150px;">
+            <img src='../../img/logo-letter-w.png' class="mt-5" alt="서비스 로고" width="180"/>
+          </b-list-group-item>
+          <b-list-group-item href="/contents/tutorial" class="text-white " style="background-color:#564892; height:70px; ">튜토리얼</b-list-group-item>
+          <b-list-group-item href="/contents/translate" class="text-white " style="background-color:#564892; height:70px; ">자막 변환</b-list-group-item>
+          <b-list-group-item href="/contents/myproject" class="text-white " style="background-color:#564892; height:70px; ">내 프로젝트</b-list-group-item>
+          <b-list-group-item href="/contents/community" class="text-white " style="background-color:#564892; height:70px; ">커뮤니티</b-list-group-item>
+          <b-list-group-item href="/contents/mypage" class="text-white " style="background-color:#564892; height:70px;">마이 페이지</b-list-group-item>
+          <!-- <b-list-group-item style="background-color:#564892; height:70px;">
+            <a href="https://github.com/eppy7819/projects/files/5048206/Translately.Tutorial.pdf"
+            class="list-group-item2download">튜토리얼 다운로드</a>
+          </b-list-group-item> -->
+          <b-list-group-item><a href="/" style="height:70px;"><b-button class="list-group-item2 mt-2" variant="light">Back To HOME</b-button></a></b-list-group-item>
+          <b-list-group-item style="background-color:#564892; margin: auto;">
+            <a href="#" @click="logout"><span><i class="fa fa-power-off text-white"></i></span>
+              <span class="text-white ">로그아웃</span>
+            </a>
+          </b-list-group-item>
+        </b-list-group>
       </div>
-
-      <ul class="list-unstyled components">
-        <li class="active">
-          <a href="/tutorial">
-          <!-- <a href="/tutorial" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"> -->
-            <i class="fas fa-home"></i>
-            튜토리얼
-          </a>
-        </li>
-        <li>
-          <a href="/myproject">
-            <i class="fas fa-briefcase"></i>
-            내 프로젝트
-          </a>
-        </li>
-        <li>
-          <a href="/articles">
-            <i class="fas fa-image"></i>
-            커뮤니티
-          </a>
-        </li>
-        <li>
-          <a href="/mypage">
-            <i class="fas fa-question"></i>
-            마이 페이지
-          </a>
-        </li>
-      </ul>
-
-      <ul class="list-unstyled CTAs">
-        <li>
-          <a href="https://bootstrapious.com/tutorial/files/sidebar.zip" class="download">Download Tutorial PDF</a>
-        </li>
-        <li>
-          <a href="/" class="article">Back To HOME</a>
-        </li>
-      </ul>
-      <ul class="list-unstyled components">
-        <li>
-          <a href="#">
-            <span><i class="fa fa-power-off"></i></span>
-            <span><router-link @click.native="logout" to="/">로그아웃</router-link></span>
-          </a>
-        </li>
-      </ul>
-    </nav>
-    <!-- Page Content  -->
-    <div class="container-fluid mt-5">
-        <div class="row justify-content-around" style="font-size: 25px; color: black;">
-            <div class="col-5">
-              <p class="font-weight-bolder" style="font-size: 25px; color: black;">영상 미리보기</p>
+      <div class="col-10" style="margin: 0;">
+        <Navbar2/>
+        <!-- Page Content  -->
+        <div class="container-fluid mt-5">
+            <div class="row justify-content-around" style="font-size: 25px; color: black;">
+                <div class="col-5">
+                  <p class="font-weight-bolder" style="font-size: 25px; color: black;">영상 미리보기</p>
+                </div>
+                <div class="col-6">
+                  <p class="font-weight-bolder" style="font-size: 25px; color: black;">자막 편집 창</p>
+                </div>
+                <div class="col-1"></div>
             </div>
-            <div class="col-7">
-              <p class="font-weight-bolder" style="font-size: 25px; color: black;">자막 편집 창</p>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-5" style="border:1px solid transparent;">
-                <!-- <p>영상</p> -->
-                <video-player-vue :video=video ></video-player-vue>
-            </div>
-            <div class="col-7" style="border:1px solid transparent; padding-right: 10px">
-              <!-- <p>자막</p> -->
-              <b-alert show dismissible fade variant="warning" style="margin-top:2px; padding-left: 20px; padding-bottom: 0px;">
-                <div class="row">
-                  <!-- <div class="col-1"><img src="..\..\img\img-thumbnail.png" alt="썸네일" style="height: 50px;"></div> -->
-                  <div class="col-10" style="padding: 0px; padding-left: 20px">
-                    <p style="color: gray; font-size: 13px; margin:0px">원래 음성의 자막이 표시됩니다.</p>
-                    <p style="color: black; font-weight: bold; margin:0px; font-size: 13px;">이 곳에서 자막을 수정할 수 있습니다.</p>
-                  </div>
-                  <div class="col-2" style="padding: 0px; padding-left: 10px;">
-                    <p style="font-size: 13px; margin-bottom: 0px;">시작 ~</p>
-                    <p style="font-size: 13px; margin-bottom: 0px;">종료</p>
+            <div class="row">
+                <div class="col-5" style="border:1px solid transparent;">
+                    <!-- <p>영상</p> -->
+                    <video-player-vue :video=video ></video-player-vue>
+                </div>
+                <div class="col-7" style="border:1px solid transparent; padding-right: 10px">
+                  <!-- <p>자막</p> -->
+                  <b-alert show dismissible fade variant="warning" style="margin-top:2px; padding-left: 20px; padding-bottom: 0px;">
+                    <div class="row">
+                      <div class="col-11" style="padding: 0px; padding-left: 20px">
+                        <p style="color: gray; font-size: 13px; margin:0px">원래 음성의 자막이 표시됩니다.</p>
+                        <p style="color: black; font-weight: bold; margin:0px; font-size: 13px;">이 곳에서 자막을 수정할 수 있습니다.</p>
+                      </div>
+                      <div class="col-1" style="padding: 0px; padding-left: 10px;">
+                        <p style="font-size: 13px; margin-bottom: 0px; color: black;">시작 ~</p>
+                        <p style="font-size: 13px; margin-bottom: 0px; color: black;">종료</p>
+                      </div>
+                    </div>
+                  </b-alert>
+                  <div :class="[isVisible ? 'bg-info' : 'bg-light', 'border', 'p-2', 'text-center']" style="height: 550px; overflow-y: scroll;">
+                    <subtitle-vue v-for="subtitle in subtitles" :key="subtitle.startTime" :subtitle="subtitle"/>  
                   </div>
                 </div>
-              </b-alert>
-              <div :class="[isVisible ? 'bg-info' : 'bg-light', 'border', 'p-2', 'text-center']" style="height: 550px; overflow-y: scroll;">
-                <subtitle-vue v-for="subtitle in subtitles" :key="subtitle.startTime" :subtitle="subtitle"/>  
               </div>
             </div>
           </div>
-        </div>
+
+
+
       </div>
+    </div>
+  </div>
+    
 
 </template>
 
@@ -97,6 +78,7 @@
   import VideoPlayerVue from "./../../components/VideoPlayerVue"
   import axios from 'axios'
   import subtitleVue from './../../components/Subtitle.vue'
+  import Navbar2 from "@/components/Navbar2.vue"
 
   const SERVER_URL = 'http://i3a511.p.ssafy.io/'
 
@@ -104,7 +86,8 @@
     name: 'CreateCaption',
     components: {
       subtitleVue,
-      VideoPlayerVue
+      VideoPlayerVue,
+      Navbar2,
     },
     props: {
       subtitles: {
@@ -123,5 +106,25 @@
 </script>
 
 <style scoped>
+.list-group-item:first-child{
+  border-top-left-radius:0rem;
+  border-top-right-radius:0rem;
+  }
+.list-group-item:last-child{
+  border-bottom-right-radius:0rem;
+  border-bottom-left-radius:0rem;
+  }
 
+.list-group-item {
+  font-size: 20px;
+  font-weight: bold;
+  background-color:#564892;
+  padding: 0px;
+  border-color: transparent;
+}
+
+.list-group-item2 {
+  font-weight: bold;
+  /* background-color: black; */
+}
 </style>
