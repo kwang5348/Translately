8f01e84bf9e0b240aa2d93e17478445c3c6691d3
hdp0545 <hdp0545@naver.com>
2020-08-13 16:06:57 +0900

    Add Login Save Token

--- vue_spring/frontend/src/http-common.js
+++ vue_spring/frontend/src/http-common.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://i3a511.p.ssafy.io:8399",
+  baseURL: "http://i3a511.p.ssafy.io:8301",
   headers: {
-    "Content-type": "application/json"
+    "Content-type": "application/json",
+    "jwt-auth-token": this.$cookies.get("auth-token")
   }
 });
