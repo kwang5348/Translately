862b0784b03f54a3c34b353dab13bc43051197e4
홍지선 <zs7819@naver.com>
2020-08-13 12:58:18 +0000

    Revert "Merge branch 'feature/login' into 'develop'"
    This reverts merge request !26

--- vue_spring/frontend/vue.config.js
+++ vue_spring/frontend/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     runtimeCompiler: true,
     devServer: {
         proxy: {
-            '/api': {target: 'http://i3a511.p.ssafy.io:8399/', ws: true, changeOrigin: true},
+            '/api': {target: 'http://i3a511.p.ssafy.io:8301/', ws: true, changeOrigin: true},
         }
     }
 }
