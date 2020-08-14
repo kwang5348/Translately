c4ddc402a0e0cc1730323d9e38a5f096446af683
hdp0545 <hdp0545@naver.com>
2020-08-14 14:09:31 +0900

    Edit subtitles

--- vue_spring/frontend/src/components/Subtitle.vue
+++ vue_spring/frontend/src/components/Subtitle.vue
@@ -6,8 +6,8 @@
           <!-- <img src="..\img\img-thumbnail.png" alt="썸네일" style="height: 50px; margin-left: 10px;"> -->
         </div>
         <div class="col-9" style="padding: 0px;">
-          <p style="color: grey; font-size: 13px; margin:0px">{{ subtitle.eng }}</p>
-          <p style="color: black; font-weight: bold; font-size: 13px;">{{ subtitle.kor }}</p>
+          <p style="color: grey; font-size: 13px; margin:0px">{{ subtitle.startsub }}</p>
+          <p style="color: black; font-weight: bold; font-size: 13px;">{{ subtitle.targetsub }}</p>
         </div>
         <div class="col-2" style="padding: 0px;">
           <!-- <p style="font-size: 13px; margin:0px">{{ subtitle.startTime }} ~</p>
@@ -35,7 +35,6 @@ export default {
       return this.subtitle.endTime.toFixed(2)
     }
   }
-
 
 }
 </script>
